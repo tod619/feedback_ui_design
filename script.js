09e019e1b4d5491b66ef750d129d41bc2a94e8b4
tod619 <trevorodwyer619@hotmail.com>
2021-04-22 19:26:04 +0100

    add eventListner on sendBtn in script.js file

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 // This project uses event bubeling instead of looping through each clickable element to add an event listner 
 const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 let selectedRating = 'Satisified'
 
-panel.addEventListener('click', (e) => {
+ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')){
         removeActive()
         e.target.parentNode.classList.add('active')
         selectedRating = e.target.nextElementSibling.innerText
         
     }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class = "fas fa-heart"></i>
+        <strong>Thank You</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support!</p>
+    `
 })
 
 function removeActive() {
